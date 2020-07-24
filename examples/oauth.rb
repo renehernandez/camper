8bdde45ebb40812fb537e18228a68d9a4b4752a0
renehernandez <renehr9102@gmail.com>
2020-07-24 08:21:53 -0400

    Update docs for retry

--- examples/oauth.rb
+++ examples/oauth.rb
@@ -6,13 +6,15 @@ Camp3.configure do |config|
     config.redirect_uri = ENV['BASECAMP3_REDIRECT_URI']
 end
 
+client = Camp3.client
+
 # Get the authorization uri
-puts Camp3.authorization_uri
+puts client.authorization_uri
 
 # Once you have received the auth code from basecamp, you can proceed with the following step
 # This will update the configuration with the corresponding tokens
 # Store the tokens in a safe place
-token = Camp3.authorize! ENV['BASECAMP3_ACCESS_CODE']
+token = client.authorize! ENV['BASECAMP3_ACCESS_CODE']
 
 # Print refresh token
 puts token.refresh_token
