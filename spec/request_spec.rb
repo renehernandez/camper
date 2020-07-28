58594ed7454c1c5b6a6505e29b35bb1e200c1c6b
renehernandez <renehr9102@gmail.com>
2020-07-28 07:20:52 -0400

    Remove extra comma

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -26,8 +26,7 @@ RSpec.describe Camp3::Request do
       body = JSON.unparse(a: 1, b: 2)
       expect(described_class.parse(body)).to be_an Camp3::Resource
       expect(described_class.parse('true')).to be true
-      expect(described_class.parse('false', )).to be false
-
+      expect(described_class.parse('false')).to be false
       expect { described_class.parse('string') }.to raise_error(Camp3::Error::Parsing)
     end
   end
