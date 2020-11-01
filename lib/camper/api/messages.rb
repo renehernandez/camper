9ea9ce66de469fbba2113413eb7595772a4e516e
Rene Hernandez <7692501+renehernandez@users.noreply.github.com>
2020-11-01 15:10:53 -0500

    Rewrite API modules to use module nesting (#63)
    * Rewrite api modules to use module nesting
    
    * Fix rubocop complaints

--- lib/camper/api/messages.rb
+++ lib/camper/api/messages.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-class Camper::Client
-  module MessagesAPI
-    def messages(message_board)
-      get(message_board.messages_url, override_path: true)
+module Camper
+  class Client
+    module MessagesAPI
+      def messages(message_board)
+        get(message_board.messages_url, override_path: true)
+      end
     end
   end
 end
